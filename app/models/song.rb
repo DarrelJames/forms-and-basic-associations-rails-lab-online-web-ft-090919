b98cd9766221a480c92245e98bd2c94c945d5f8a
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-03 20:36:38 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,27 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    # code
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each do | note |
+      if note.strip != ""
+        self.notes.build(content: note)
+      end
+    end
+  end
+
+    def note_contents
+      self.notes.map { |note| note.content  }
+    end
 end
